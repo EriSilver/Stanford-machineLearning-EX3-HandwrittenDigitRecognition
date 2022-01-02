6e089d9d74a24e7f3bf5bb1e73ca49432b545952
silver <dr.george.erini@gmail.com>
2022-01-02 13:27:48 +0200

    ALL DONE! Hand-written recognition for numbers! :) Neural Network and multi-class classification (one vs all method)
      % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
                                     Dload  Upload   Total   Spent    Left  Speed
    100  1550  100   933  100   617   1285    849 --:--:-- --:--:-- --:--:--  2134
    ==
    ==                                   Part Name |     Score | Feedback
    ==                                   --------- |     ----- | --------
    ==             Regularized Logistic Regression |  30 /  30 | Nice work!
    ==              One-vs-All Classifier Training |  20 /  20 | Nice work!
    ==            One-vs-All Classifier Prediction |  20 /  20 | Nice work!
    ==          Neural Network Prediction Function |  30 /  30 | Nice work!
    ==                                   --------------------------------
    ==                                             | 100 / 100 |
    ==

--- predict.m
+++ predict.m
@@ -21,13 +21,16 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+a1 = [ones(size(X,1), 1), X];
 
+a2 = sigmoid(a1 * Theta1');
 
+z3 = [ones(size(a2,1), 1), a2] * Theta2';
+a3 = sigmoid(z3);
 
+[maxes, p] = max(a3');
 
-
-
-
+p = p';
 
 % =========================================================================
 
